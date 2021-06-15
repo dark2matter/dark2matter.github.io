2b8f29b0b15172377b97a00f468b23f4ec6aae5f
skulluglify <skulluglify@outlook.com>
2021-06-15 12:45:10 +0700

    +change download_userface.sh users.csv fixed

--- extras/teamworks/download_userface.sh
+++ extras/teamworks/download_userface.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
 
 for x in `cat users.csv`; do
-    username=$(echo $x | cut -d ',' -f1)
-    userface=$(echo $x | cut -d ',' -f2)
-    if [ "$(echo $userface | grep -Eiv 'NONE')" ]; then
-        wget -c $userface -A jpeg,jpg,bmp,gif,png -O images/$username\.png
+    if [ "$(echo $x | grep -Eiv '(^USERNAME|USERID$)')" ]; then
+        username=$(echo $x | cut -d ',' -f1)
+        usernoid=$(echo $x | cut -d ',' -f2)
+        if [ "$(echo $usernoid | grep -Eiv 'NONE')" ]; then
+            # wget -c  "https://avatars.githubusercontent.com/u/${usernoid}?s=96&v=4" -A jpeg,jpg,bmp,gif,png -O images/${username}\.png
+            wget -c  "https://avatars.githubusercontent.com/u/${usernoid}?s=96&v=4" -A png -O images/${username}\.png
+        fi
     fi
 done
